92317caf2d265964bab0de65e9b3cc9a902b7562
Charlie <chen_610146@hotmail.com>
2021-04-02 13:36:03 +0800

    change

--- owl-carousel.js
+++ owl-carousel.js
@@ -4,7 +4,6 @@ const q = k.split('/')
 if (q[1] !== 'admin') {
   $('.carousel').addClass("owl-carousel owl-theme")
 
-  console.log('front-end')
   $('.owl-carousel').owlCarousel(OwlCarousel)
 
 } else {
