336ce2175522d0074d9370ef615a1a9932ffd613
Charlie <chen_610146@hotmail.com>
2021-04-02 13:29:59 +0800

    parameter out

--- main.js
+++ main.js
@@ -5,24 +5,7 @@ if (q[1] !== 'admin') {
   $('.carousel').addClass("owl-carousel owl-theme")
 
   console.log('front-end')
-  $('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    autoplay: true,
-    nav: false,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      600: {
-        items: 3
-      },
-      1000: {
-        items: 5
-      }
-    }
-  })
+  $('.owl-carousel').owlCarousel(OwlCarousel)
 
 } else {
   $('.carousel').removeClass("owl-carousel owl-theme")
